f427df460399a50155b095a74fcef5a2f9ee6ed8
Nate8888 <wilk.nathan@gmail.com>
2020-05-17 02:06:20 -0400

    hedera full connection + hash working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import the Hedera Hashgraph JS SDK
 // Example uses Hedera JavaScript SDK v1.1.8
-const { Client, CryptoTransferTransaction, AccountId } = require("@hashgraph/sdk");
+const { Client, CryptoTransferTransaction, AccountId, AccountBalanceQuery  } = require("@hashgraph/sdk");
 // Allow access to our .env file variables
 require("dotenv").config();
 
@@ -24,47 +24,60 @@ client.setOperator(operatorAccountId, operatorPrivateKey);
 
 // Hedera is an asynchronous environment :)
 (async function() {
-    console.log("balance before transfer:", (await client.getAccountBalance(operatorAccountId)));
+    //console.log("balance before transfer:", (await client.getAccountBalance(operatorAccountId)));
+    let to
+    let stickerHash
+    if(process.argv.length > 2){
+        const to = process.argv[2];
+        const stickerHash = process.argv[3];
 
-    const transactionId = await (await new CryptoTransferTransaction()
-        .addSender(operatorAccountId, 1)
-        .addRecipient("0.0.3", 1)
-        .setTransactionMemo("StickerChain HASH")
-        .execute(client))
-
-
-        console.log(`attempting to get receipt for transaction id = ${transactionId}\n`);
-
-        const hexTransactionIDForRequest = Buffer.from(`${transactionId}`, 'utf8').toString('hex');
-        console.log(hexTransactionIDForRequest)
-        const from = operatorAccountId
-        console.log(from)
-
-        //console.log(process.argv);
-        // TODO:::: GET command line input
-        const to = process.argv
-        const stickerHash = process.argv
-
-        const receipt = await transactionId.getReceipt(client);
-
-        console.log(`transaction ${
-            transactionId
-        } receipt = ${
-            JSON.stringify(receipt)
-        }\n`);
+        const transactionId = await (await new CryptoTransferTransaction()
+            .addSender(operatorAccountId, 1)
+            .addRecipient(to, 1)
+            .setTransactionMemo("StickerChain "+stickerHash)
+            .execute(client))
 
 
-    console.log(transactionId);
-    console.log(receipt.getAccountId)
+            //console.log(`attempting to get receipt for transaction id = ${transactionId}\n`);
+
+            const hexTransactionIDForRequest = Buffer.from(`${transactionId}`, 'utf8').toString('hex');
+            //console.log(hexTransactionIDForRequest);
+            const from = operatorAccountId;
+            //console.log(from);
+
+            //onsole.log(process.argv);
+            // TODO:::: GET command line input
 
 
-    console.log(`attempting to get record for transaction id = ${transactionId}\n`);
-    const record = await transactionId.getRecord(client);
-    console.log(`transaction ${
-        transactionId
-    } record = ${
-        JSON.stringify(record)
-    }\n`);
-    console.log("balance after transfer:", (await client.getAccountBalance(operatorAccountId)));
+              const receipt = await transactionId.getReceipt(client);
 
+              console.log(`transaction ${
+                  transactionId
+              } receipt = ${
+                  JSON.stringify(receipt)
+              }\n`);
+
+
+          console.log(transactionId);
+          console.log(receipt.getAccountId);
+
+
+          console.log(`attempting to get record for transaction id = ${transactionId}\n`);
+          const record = await transactionId.getRecord(client);
+          console.log(`transaction ${
+              transactionId
+          } record = ${
+              JSON.stringify(record)
+          }\n`);
+
+          const balance = await new AccountBalanceQuery()
+            .setAccountId(operatorAccountId)
+            .execute(client);
+
+          console.log(`${operatorAccountId} balance = ${balance.asTinybar()}`);
+  }
+  else{
+    console.log("EXIT(); -> Program Usage: \"node index.js ToAccount ImgHASH\"");
+  }
+process.exit()
 }());
